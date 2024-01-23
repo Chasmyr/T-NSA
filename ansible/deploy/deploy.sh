6d6069a7aaf785e1d34c49c4d3814f975d0a6d49
chasmyr <lestratgabin5886@gmail.com>
2024-01-23 10:26:57 +0100

    first

---
+++ ansible/deploy/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# TODO: Ajoutez des commandes de déploiement spécifiques, par exemple :
+# 1. Cloner le dépôt Git sur le serveur
+# 2. Installer les dépendances
+# 3. Construire et déployer l'application
+
+# Exemple :
+# git clone <url_du_repo> /chemin/vers/application
+# cd /chemin/vers/application
+# npm install
+# npm run build
+# cp -R /chemin/vers/application /var/www/html/
+
+# Vous pouvez également utiliser des variables d'environnement si nécessaire.
+# Exemple : $ENV_VARIABLE
+
+# N'oubliez pas de rendre le script exécutable :
+# chmod +x deploy.sh
